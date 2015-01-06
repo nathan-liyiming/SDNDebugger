fe0e454b2fa927151bcb464db933a55693ed7ef0
nathan-liyiming <nathan.liyiming@gmail.com>
2015-01-06 10:07:52 -0500

    mininet py added

--- SDNDebugger/run.sh
+++ SDNDebugger/run.sh
@@ -2,7 +2,7 @@ xterm -T "debugger" -e java -cp "bin:lib/*" net.sdn.debugger.Debugger &
 sleep 1
 xterm -T "proxy" -e java -cp "bin:lib/*" net.sdn.proxy.Proxy &
 sleep 1
-xterm -T "mininet" -e mn --topo=linear,2 --controller=remote,ip=127.0.0.1,port=8000 --switch=ovsk,protocols=OpenFlow13 &
+xterm -T "mininet" -e mn/run_mininet.py linear,2 &
 sleep 10
 xterm -T "monitor" -e java -cp "bin:lib/*" net.sdn.monitor.Monitor &
 
