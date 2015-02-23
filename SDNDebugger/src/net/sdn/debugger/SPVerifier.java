711631aa44a4d35713bfa3ce1c6989b456f77753
nathan-liyiming <nathan.liyiming@gmail.com>
2015-02-23 15:50:31 -0500

    check counting hops of SP

--- SDNDebugger/src/net/sdn/debugger/SPVerifier.java
+++ SDNDebugger/src/net/sdn/debugger/SPVerifier.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 
 import net.sdn.event.Event;
-import net.sdn.event.EventGenerator;
 import net.sdn.event.packet.PacketType;
 import net.sdn.event.util.BellmanFord;
 import net.sdn.event.util.BellmanFord.Triple;
@@ -14,6 +13,7 @@ import net.sdn.phytopo.Switch;
 public class SPVerifier extends Verifier {
 	private PhyTopo phyTopo = null;
 	private HashMap<BellmanFord.Pair, List<Triple>> routes = null;
+	private HashMap<BellmanFord.Pair, Integer> count = new HashMap<BellmanFord.Pair, Integer>();
 
 	public SPVerifier(PhyTopo phytopo) {
 		phyTopo = phytopo;
@@ -26,27 +26,56 @@ public class SPVerifier extends Verifier {
 		String nw_src = event.pkt.eth.ip.nw_src;
 		String nw_dst = event.pkt.eth.ip.nw_dst;
 		Switch s = phyTopo.getSwitch(event.sw);
-		
+
+		BellmanFord.Pair b = new BellmanFord.Pair(nw_src, nw_dst);
 		// check whether the event is in
-		if (event.direction.equalsIgnoreCase("in")){
+		if (event.direction.equalsIgnoreCase("in")) {
 			// get all attached hosts
 			for (String host : s.getAttachedHosts()) {
 				// first hop, generate events
 				if (phyTopo.getHost(host).getNwAddr().equalsIgnoreCase(nw_src)) {
-					List<Triple> route = routes.get(new BellmanFord.Pair(nw_src, nw_dst));
-					for (int i = 1; i < route.size(); i++){
-						Triple hop = route.get(i);
-						addExpectedEvents(EventGenerator.generateEvent(
-								event.priority, event.pkt, hop.sw.getId(), hop.interf, hop.direction, event.timeStamp));
+					List<Triple> route = routes.get(b);
+					count.put(b, route.size() - 1);
+					return;
+				}
+			}
+
+			if (count.containsKey(b)) {
+				count.put(b, count.get(b) - 1);
+			} else {
+				System.out.println("Unknown event: " + event);
+			}
+		}
+
+		// check whether the event is out
+		if (event.direction.equalsIgnoreCase("out")) {
+			// get all attached hosts
+			for (String host : s.getAttachedHosts()) {
+				// final hop, generate events
+				if (phyTopo.getHost(host).getNwAddr().equalsIgnoreCase(nw_dst)) {
+					if (count.containsKey(b) && count.get(b) == 1) {
+						System.out
+								.println("Correct, this is one shortest path from "
+										+ nw_src
+										+ " to "
+										+ nw_dst
+										+ " for "
+										+ event.pkt.eth.ip.icmp.op + "!");
+					} else {
+						System.out.println("This is not shortest path!");
 					}
 					return;
 				}
 			}
+
+			if (count.containsKey(b)) {
+				count.put(b, count.get(b) - 1);
+			} else {
+				System.out.println("Unknown event: " + event);
+			}
 		}
-		
-		checkEvents(event);
 	}
-	
+
 	public HashMap<BellmanFord.Pair, List<Triple>> calcSP() {
 		return BellmanFord.bellmanFordCompute(phyTopo);
 	}
@@ -55,7 +84,7 @@ public class SPVerifier extends Verifier {
 		Verifier v = new SPVerifier(new PhyTopo(args[0]));
 		v.addInterestedEvents(PacketType.TCP);
 		v.addInterestedEvents(PacketType.ICMP);
-		//v.addInterestedEvents(PacketType.UDP);
+		// v.addInterestedEvents(PacketType.UDP);
 		new Thread(v).start();
 	}
 
