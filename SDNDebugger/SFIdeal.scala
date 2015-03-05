c9160f14b5c07d9cbb9c24fd85c52f3a7be51ad4
Tim Nelson <tn@cs.brown.edu>
2015-03-04 21:30:39 -0500

    sketching ideal model

--- SDNDebugger/SFIdeal.scala
+++ SDNDebugger/SFIdeal.scala
@@ -20,7 +20,42 @@
 import net.sdn.phytopo.Host;
 import net.sdn.phytopo.PhyTopo;
 import net.sdn.phytopo.Switch;
+import net.sdn.event._;
+import net.sdn.event.packet._;
 
 class SFIdeal(topofn: String) {
-	def topo = new PhyTopo(topofn);
+	val topo = new PhyTopo(topofn);
+	val allowed = new scala.collection.mutable.TreeSet[Tuple2[String, String]]();
+
+	// TODO: this is way too verbose
+	def stateHelper(e: Event): Option[Tuple2[String, String]] = {
+		e match {
+			case et: NetworkEvent => {
+					if(et.pkt.eth == null) return None
+					else return Some(new Tuple2[String,String](et.pkt.eth.dl_src, et.pkt.eth.dl_dst))
+			}
+			case _ => return None
+		}
+	}
+
+	def printwarning(e: Event) {
+		println("**** Expectation violated!");
+		println(e); // TODO more detail
+	}
+
+	// Listen in for necessary state changes
+	Simon.rememberInSet[Tuple2[String,String]](Simon.events(), allowed, stateHelper);
+
+	// ext -> int [not in state] => expect dropped
+
+	// int -> ext => expect pass, add to state
+	// TODO: is flatMap safe here?
+	Simon.events().filter(is_incoming_int).flatMap(e =>
+		e match
+			case et: NetworkEvent =>
+				Simon.expect({e2 => /* outgoing, same sw, same addrs... et.pkt.eth.dl_src*/}, Duration(10, "milliseconds"))).subscribe(printwarning);
+
+
+
+	// ext -> int [in state] => expect pass
 }
