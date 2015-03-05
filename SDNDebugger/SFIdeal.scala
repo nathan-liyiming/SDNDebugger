9aa7be7a7e9d8e86ad559abb12843d6b4793180b
Tim Nelson <tn@cs.brown.edu>
2015-03-04 20:11:29 -0500

    fix uncompiling code

--- SDNDebugger/SFIdeal.scala
+++ SDNDebugger/SFIdeal.scala
@@ -1,16 +1,26 @@
 /*
 	Ideal model + reactor for Stateful Firewall example
 
-	
+
 	Could be enhanced to use cancellable expectations; right now this model
-	will be confused by an over-eager external host.  Consider 
+	will be confused by an over-eager external host.  Consider
 		(1) ext -> [expect not to see corresponding output]
-		(2) int -> [expect to pass, modify state;cancel original expectation (assuming no way to distinguish (1) and (3))] 
+		(2) int -> [expect to pass, modify state;cancel original expectation (assuming no way to distinguish (1) and (3))]
 		(3) ext -> [expect to pass]
 
 */
 
-object SFIdeal {
-	
+/*
+	With respect to a topology that defines a set of SFW switches and, for each switch, a set of internal and external ports...
+		ext -> int [not in state] => expect dropped
+		int -> ext => expect pass, add to state
+		ext -> int [in state] => expect pass
+*/
 
+import net.sdn.phytopo.Host;
+import net.sdn.phytopo.PhyTopo;
+import net.sdn.phytopo.Switch;
+
+class SFIdeal(topofn: String) {
+	def topo = new PhyTopo(topofn);
 }
--- SDNDebugger/Simon.scala
+++ SDNDebugger/Simon.scala
@@ -16,14 +16,14 @@ import rx.lang.scala.JavaConversions;
 import scala.collection.mutable.SortedSet;
 
 // Note on adding new classes that extend Event:
-//  Event.toString() uses GSON to produce a JSON expression for the event. 
+//  Event.toString() uses GSON to produce a JSON expression for the event.
 //  Unfortunately this doesn't work well for Scala classes that inherit the method.
 //  If you get a malformed class name error, make sure you've overridden the toString() method.
 
 object Simon {
 	var d: Debugger = new Debugger();
 	private var running = false;
-	
+
 	def run() {
 		if(running) return;
 		println("Creating debugger object and opening monitor listener...");
@@ -31,16 +31,16 @@ object Simon {
 		running = true;
 		// Dont make a fixed variable! Will never be updated when monitors join,
 		// which will happen after this function terminates...
-		// events = JavaConversions.toScalaObservable(d.events)		
+		// events = JavaConversions.toScalaObservable(d.events)
 	}
 
 	// WARNING: this won't auto-update existing Observables as new monitors join,
-	// so wait until monitor registration is complete before calling (or be 
+	// so wait until monitor registration is complete before calling (or be
 	// aware that you may need to reconstruct streams you've already built.)
-	// NOTE: this should be a *hot* Observable, i.e., it doesn't save events 
-	// that have happened before subscription. 	
-	def events(): Observable[Event] = {		
-		JavaConversions.toScalaObservable(d.events)		
+	// NOTE: this should be a *hot* Observable, i.e., it doesn't save events
+	// that have happened before subscription.
+	def events(): Observable[Event] = {
+		JavaConversions.toScalaObservable(d.events)
 	}
 
 	// TODO: ideally we'd have another function that created a new xterm and ran a continuous monitor until completion
@@ -61,7 +61,7 @@ object Simon {
 		EXPECTATIONS
 
 		Expect to see an event matching pred within duration d.
-		If this isn't seen after d, result contains an ExpectViolation. 
+		If this isn't seen after d, result contains an ExpectViolation.
 		If this is seen before d, result contains just the event that matched.
 
 		Note that expectation observables can be re-used. Re-subscribe to start a fresh timer and resume listening.
@@ -70,25 +70,25 @@ object Simon {
 	def expect(pred: Event=>Boolean, d: Duration): Observable[Event] = {
 		return expect(pred, d, Observable.never)
 	}
-	// Cancellable expectation. 
+	// Cancellable expectation.
 	// If the cancel observable fires an event, this expectation will never fire either way.
 	def expect(pred: Event=>Boolean, d: Duration, cancel: Observable[AnyVal]): Observable[Event] = {
-		// timer, filter components 
+		// timer, filter components
 		val t = Observable.timer(d).map(n => new ExpectViolation());
-		val f = events().filter(pred); 		
-		return t.merge(f).first.takeUntil(cancel); 
-	}	
+		val f = events().filter(pred);
+		return t.merge(f).first.takeUntil(cancel);
+	}
 
-	/* 
-		State management: ideal models may have internal state. 
-		We may also just want to store info extracted from a stream. 		
+	/*
+		State management: ideal models may have internal state.
+		We may also just want to store info extracted from a stream.
 	*/
-	
-	def rememberInSet[T](o: Observable[Event], s: mutable.SortedSet[T], f: Event=>Option[T]): Subscription = {
+
+	def rememberInSet[T](o: Observable[Event], s: scala.collection.mutable.SortedSet[T], f: Event=>Option[T]): Subscription = {
 		return o.subscribe({e => f(e) match { case Some(t) => s += t case None => ()}})
 	}
 
-	
+
 /*
 // prints if expectation violated
 Simon.expect({e:Event => e.direction == "in"}, Duration(10, "seconds")).subscribe(e => println("result: "+e))
@@ -125,4 +125,4 @@ scala> Simon.run()
 */
 
 
-		
+
