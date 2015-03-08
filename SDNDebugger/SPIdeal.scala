e27ebf5e439a03365ac7f079ae3da56d7206e5cf
nathan-liyiming <nathan.liyiming@gmail.com>
2015-03-08 15:25:38 -0400

    fix the syntax in SPIdeal

--- SDNDebugger/SPIdeal.scala
+++ SDNDebugger/SPIdeal.scala
@@ -4,17 +4,32 @@ import net.sdn.phytopo.Switch
 import net.sdn.event._
 import net.sdn.event.packet._
 import net.sdn.event.util.BellmanFord
-import net.sdn.event.util.BellmanFord.Triple
+import net.sdn.event.util.BellmanFord.Pair
 import scala.collection.mutable.Map
+import scala.collection.JavaConversions._
 
 class SPIdeal(topofn: String) {
 	val topo = new PhyTopo(topofn)
 	val routes = BellmanFord.bellmanFordCompute(topo)
 	val count = Map[BellmanFord.Pair, Int]()
 
+	def printwarning(e: Event) {
+		e match {
+			case eviol: ExpectViolation => 
+				println("**** Violation: ****")
+				println(eviol)
+			case esucc: ExpectSuccess =>
+				println("**** Success: ****") 
+				println(esucc)
+			case _ =>
+				println("**** Unknown: ****")
+				println(e)
+		}
+	}
+
 	def isPassThrough(orig: NetworkEvent): NetworkEvent => Boolean = {
 		{e => {
-			val b = BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
+			val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
 			count(b) = count(b) - 1
 			e.pkt.eth.ip.nw_src == orig.pkt.eth.ip.nw_src &&
 			e.pkt.eth.ip.nw_dst == orig.pkt.eth.ip.nw_dst &&
@@ -23,10 +38,10 @@ class SPIdeal(topofn: String) {
 		}
 	}
 
-	def isFirstHop(e: NetworkEvent): Boolean {
+	def isFirstHop(e: NetworkEvent): Boolean = {
 		if (e.direction == NetworkEventDirection.IN) {
 			for (host <- topo.getSwitch(e.sw).getAttachedHosts()) {
-				if (topo.getHost(host).getNwAddr() == e.nw_src) {
+				if (topo.getHost(host).getNwAddr() == e.pkt.eth.ip.nw_src) {
 					return true
 				}
 			}
@@ -34,10 +49,10 @@ class SPIdeal(topofn: String) {
 		return false
 	}
 
-	def isLastHop(e: NetworkEvent): Boolean {
+	def isLastHop(e: NetworkEvent): Boolean = {
 		if (e.direction == NetworkEventDirection.OUT) {
 			for (host <- topo.getSwitch(e.sw).getAttachedHosts()) {
-				if (topo.getHost(host).getNwAddr() == e.nw_dst) {
+				if (topo.getHost(host).getNwAddr() == e.pkt.eth.ip.nw_dst) {
 					return true
 				}
 			}
@@ -46,22 +61,25 @@ class SPIdeal(topofn: String) {
 	}
 
 	def setCounter(e: NetworkEvent) {
-		val b = BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
+		val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
 		count(b) = routes.get(b).size() / 2
 	}
 
 	def isCounterZero(e: NetworkEvent): Boolean = {
-		val b = BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
+		val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
 		count(b) == 0
 	}
 
 	val ICMPStream = Simon.nwEvents().filter(SimonHelper.isICMPNetworkEvents)
 
-	ICMPStream.filter(isFirstHop).subscribe(setCounter)
+	ICMPStream.filter(isFirstHop).subscribe(setCounter(_))
 
-	ICMPStream.filter(e => e.direction == NetworkEventDirection.IN).flatMap(e =>
+	val e1 = ICMPStream.filter(e => e.direction == NetworkEventDirection.IN).flatMap(e =>
 				Simon.expect(ICMPStream, isPassThrough(e), Duration(100, "milliseconds")));
 
-	ICMPStream.filter(isLastHop).flatMap(e =>
+	val e2 = ICMPStream.filter(isLastHop).flatMap(e =>
 				Simon.expect(ICMPStream, isCounterZero, Duration(100, "milliseconds")))
+
+	val violations = e1.merge(e2)
+	val autosubscribe = violations.subscribe(printwarning(_))
 }
