9e5cd49f737e0b0ad4cc72118aac2cb7e3dafd96
nathan-liyiming <nathan.liyiming@gmail.com>
2015-03-08 22:41:11 -0400

    SP scala ideal has been tested

--- SDNDebugger/SPIdeal.scala
+++ SDNDebugger/SPIdeal.scala
@@ -8,6 +8,8 @@ import net.sdn.event.util.BellmanFord.Pair
 import scala.collection.mutable.Map
 import scala.collection.JavaConversions._
 
+import rx.lang.scala.Observable
+
 class SPIdeal(topofn: String) {
 	val topo = new PhyTopo(topofn)
 	val routes = BellmanFord.bellmanFordCompute(topo)
@@ -27,10 +29,12 @@ class SPIdeal(topofn: String) {
 		}
 	}
 
+	def isComingIn(e: NetworkEvent): Boolean = {
+		e.direction == NetworkEventDirection.IN
+	}
+
 	def isPassThrough(orig: NetworkEvent): NetworkEvent => Boolean = {
 		{e => {
-			val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
-			count(b) = count(b) - 1
 			e.pkt.eth.ip.nw_src == orig.pkt.eth.ip.nw_src &&
 			e.pkt.eth.ip.nw_dst == orig.pkt.eth.ip.nw_dst &&
 			e.sw == orig.sw && e.direction == NetworkEventDirection.OUT
@@ -53,6 +57,7 @@ class SPIdeal(topofn: String) {
 		if (e.direction == NetworkEventDirection.OUT) {
 			for (host <- topo.getSwitch(e.sw).getAttachedHosts()) {
 				if (topo.getHost(host).getNwAddr() == e.pkt.eth.ip.nw_dst) {
+					val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
 					return true
 				}
 			}
@@ -62,24 +67,28 @@ class SPIdeal(topofn: String) {
 
 	def setCounter(e: NetworkEvent) {
 		val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
-		count(b) = routes.get(b).size() / 2
+		if (isFirstHop(e)) {
+			count(b) = routes.get(b).size() / 2
+		}
+		
+		count(b) = count(b) - 1
 	}
 
-	def isCounterZero(e: NetworkEvent): Boolean = {
+	def expectCounterZero(e: NetworkEvent): Event = {
 		val b = new BellmanFord.Pair(e.pkt.eth.ip.nw_src, e.pkt.eth.ip.nw_dst)
-		count(b) == 0
+		count(b) == 0 match {case true => new ExpectSuccess() case false => new ExpectViolation(e)}
 	}
 
 	val ICMPStream = Simon.nwEvents().filter(SimonHelper.isICMPNetworkEvents)
 
-	ICMPStream.filter(isFirstHop).subscribe(setCounter(_))
+	ICMPStream.filter(isComingIn).subscribe(setCounter(_))
 
-	val e1 = ICMPStream.filter(e => e.direction == NetworkEventDirection.IN).flatMap(e =>
+	val e1 = ICMPStream.filter(isComingIn).flatMap(e =>
 				Simon.expect(ICMPStream, isPassThrough(e), Duration(100, "milliseconds")));
 
-	val e2 = ICMPStream.filter(isLastHop).flatMap(e =>
-				Simon.expect(ICMPStream, isCounterZero, Duration(100, "milliseconds")))
+	val e2 = ICMPStream.filter(isLastHop).map(expectCounterZero(_))
 
 	val violations = e1.merge(e2)
+	
 	val autosubscribe = violations.subscribe(printwarning(_))
 }
